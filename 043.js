727e42670a27ad16cdd10846caefaf00a1433a5c
unknown <ee06b056@gmail.com>
2018-08-20 11:50:31 -0400

    043 first solution

--- 043.js
+++ 043.js
@@ -4,16 +4,57 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
-    
+    if (num1 == '0' || num2 == '0') {
+        return '0';
+    }
+    let ans = '0', temp = '';
+    for (let i = num2.length - 1; i >= 0; i--) {
+        ans = addBystr(ans, multiplyByDigit(num1, num2[i]) + temp);
+        temp += '0';
+    }
+    return ans;
 };
 
 let addBystr = function (num1, num2) {
     let flag = 0, ans = '';
-    let i = 0;
-    while (i < num1.length && i < num2.length) {
-        let a = parseInt(num1[i]), b = parseInt(num2[i]);
-        let halfSum = (a + b + flag) % 10 + '';
-        flag = Math.floor((a + b) / 10);
-        ans = halfSum + ans;
+    let i = num1.length - 1, j = num2.length - 1;
+    while (i >= 0 || j >= 0) {
+        let a = i >= 0 ? parseInt(num1[i]) : 0;
+        let b = j >= 0 ? parseInt(num2[j]) : 0;
+        let subSum = String((a + b + flag) % 10);
+        ans = subSum + ans;
+        flag = Math.floor((a + b + flag) / 10);
+        i--;
+        j--;
     }
+    if (flag != 0) {
+        ans = String(flag) + ans;
+    }
+    return ans;
 }
+
+let multiplyByDigit = function (num1, num2) {
+    if (num2 == '0') {
+        return '0';
+    }
+    let flag = 0, ans = '', b = parseInt(num2);
+    for (let i = num1.length - 1; i >= 0; i--) {
+        let a = parseInt(num1[i]);
+        let halfMultiply = String((a * b + flag) % 10);
+        ans = halfMultiply + ans;
+        flag = Math.floor((a * b + flag) / 10);
+    }
+    if (flag != 0) {
+        ans = String(flag) + ans;
+    }
+    return ans;
+}
+
+// better answer
+var multiply = function(num1, num2) {
+    if (num1 == '0' || num2 == '0') {
+        return '0';
+    }
+    let n1 = num1.split(''), n2 = num2.split('');
+    
+};
