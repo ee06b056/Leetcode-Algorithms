0a8cc18f2845d8cd5d67a4dbae27a6850775febd
Bo Li <ee06b056@users.noreply.github.com>
2018-09-18 16:26:52 -0400

    no progress

--- 065_to_do.js
+++ 065_to_do.js
@@ -3,5 +3,9 @@
  * @return {boolean}
  */
 var isNumber = function(s) {
+    const sArr = s.trim().split('e');
+    if (sArr.length > 2 || sArr.length < 1) {
+        return false;
+    }
     
 };
--- 115_to_do.js
+++ 115_to_do.js
@@ -3,6 +3,31 @@
  * @param {string} t
  * @return {number}
  */
+
+//  recursive 
 var numDistinct = function(s, t) {
+    function _numDistinct (s, t, i, j) {
+        if (j == t.length && i <= s.length) {
+            ans++;
+            return;
+        } else if (i == s.length && j < t.length) {
+            return;
+        }
+        for (let k = i; k < s.length; k++) {
+            if (s[k] == t[j]) {
+                _numDistinct(s, t, k+1, j+1);
+            }
+        }
+    }
+    let ans = 0;
+    _numDistinct(s, t, 0, 0);
+    return ans;
+};
+
+
+var numDistinct = function (s, t) {
     
 };
+
+let s = 'babgbag', t = 'bag';
+console.log(numDistinct(s, t));
